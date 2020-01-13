7ea694ee71271d8fec8072fd911891115fcf3048
Nathan Mousa <nathanmousa1@gmail.com>
2020-01-13 18:41:36 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  
-  def up
-  end
-
-  def down
-  end
+class CreateArtist < ActiveRecord::Migration[5.2]
 
   def change
     create_table :artists do |t|
